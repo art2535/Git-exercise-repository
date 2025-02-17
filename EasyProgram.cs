3a7d20bd28a5447563cbfbdd929661ea94e5df28
art2535 <artem010606@yandex.ru>
2025-02-17 21:40:59 +0300

    Написана новая программа

--- EasyProgram.cs
+++ EasyProgram.cs
@@ -1,1 +1,11 @@
-// хочу пельмени!
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введите ваше имя: ");
+        string name = Console.ReadLine();
+        Console.WriteLine($"Привет, {name}!");
+    }
+}
